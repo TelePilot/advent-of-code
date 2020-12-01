3dd0136a4dc08e64d358f788247e50f9efce38a9
blixt <teodor.blixt@gmail.com>
2020-12-01 21:41:04 +0100

    day1

---
+++ day1/script.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+var fs = require('fs')
+
+fs.readFile('text.txt', 'utf8', function (err, data) {
+	if (err) throw err
+	let array = data.split('\n').map(item => parseInt(item, 10))
+	//Solution 1
+	loop1: for (let i = 0; i < array.length; i++) {
+		const value = array[i]
+		for (let i2 = 0; i2 < array.length; i2++) {
+			const value2 = array[i2]
+			if (value + value2 === 2020) {
+				console.log(value, value2, value * value2)
+				break loop1
+			}
+		}
+	}
+	//Solution 2
+	loop2: for (let i = 0; i < array.length; i++) {
+		const value = array[i]
+		for (let i2 = 0; i2 < array.length; i2++) {
+			const value2 = array[i2]
+			for (let i3 = 0; i3 < array.length; i3++) {
+				const value3 = array[i3]
+				if (value + value2 + value3 === 2020) {
+					console.log(value, value2, value3, value * value2 * value3)
+					break loop2
+				}
+			}
+		}
+	}
+})
+
+const port = 3000
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
