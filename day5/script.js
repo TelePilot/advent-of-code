02b3fdb1f80f278eb8d35c74d8b420aa1a5ddfbd
blixt <teodor.blixt@gmail.com>
2020-12-06 03:33:29 +0100

    day5 solution

--- day5/script.js
+++ day5/script.js
@@ -5,17 +5,20 @@ fs.readFile('text.txt', 'utf8', function (err, data) {
 	let array = data.split('\n')
 	seatNumber(array)
 })
-const getColumn = (seat, min, max, minChar, maxChar) => {
+const getColumn = (seat, min, max, leftChar) => {
 	let minS = min
 	let maxS = max
 	let seatColumn = 0
 	seat.split('').forEach((char, idx) => {
 		if (idx === seat.length - 1) {
-			seatColumn = char === minChar ? minS : maxS - 1
+			seatColumn = char === leftChar ? minS : maxS - 1
 		} else {
 			let diff = (maxS - minS) / 2
-			if (char === minChar) seatColumn = maxS -= diff
-			if (char === maxChar) seatColumn = minS += diff
+			if (char === leftChar) {
+				seatColumn = maxS -= diff
+			} else {
+				seatColumn = minS += diff
+			}
 		}
 	})
 	return seatColumn
@@ -23,18 +26,21 @@ const getColumn = (seat, min, max, minChar, maxChar) => {
 const seatNumber = data => {
 	const seat = []
 	data.forEach(ticket => {
-		let row = getColumn(ticket.substring(0, 7), 0, 128, 'F', 'B')
-		let column = getColumn(ticket.substring(7), 0, 8, 'L', 'R')
+		let row = getColumn(ticket.substring(0, 7), 0, 128, 'F')
+		let column = getColumn(ticket.substring(7), 0, 8, 'L')
 		seat.push([row, column])
 	})
 
-	const high = seat.map((item, idx) => item[0] * 8 + item[1])
-	console.log(Math.max(...high))
+	const high = seat.map(item => item[0] * 8 + item[1])
+
 	const missing = []
-	for (var i = 1; i <= sorted.length; i++) {
+	for (var i = 1; i <= high.length; i++) {
 		if (high.indexOf(i) == -1) {
 			missing.push(i)
 		}
 	}
+	//solution 1
+	console.log(Math.max(...high))
+	//solution 2
 	console.log(missing)
 }
